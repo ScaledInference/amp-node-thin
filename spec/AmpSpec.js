05f1f1a8e6ac358a5cbd46dba5a061c3a9301153
Steve Mask <steve@scaledinference.com>
2017-08-17 13:26:55 -0700

    Updated tests

--- spec/AmpSpec.js
+++ spec/AmpSpec.js
@@ -9,24 +9,42 @@ describe("Amp", function(){
 
     it("should make observe call to Amp agent", function(done) {
         session.observe("ObserveTest", {"tao":"awesome"}, {}, function(error, response) {
-            if (error) { console.log(error); }
+            if (error) { 
+                console.log(error); 
+                fail();
+            }
             console.log(response);
+            expect(response).toContain("OK");
             done();
         });
     });
 
     it("should make decide call to Amp agent and return array with single candidate", function(done) {
-        session.decide("DecideTest", {"tao":["awesome", "ok", "worthless"]}, {limit: 1}, function(error, response) {
-            if (error) { console.log(error); }
+        session.decide("DecideTest", {"tao":["awesome", "ok", "worthless"]}, {limit: 1}, function(error, decision, response) {
+            if (error) { 
+                console.log(error); 
+                fail();
+            }
             console.log(response); // response should be array
+
+            expect(decision.length).toEqual(1);
+            expect(decision[0].tao).toEqual("awesome");
+
             done();
         });
     });
 
     it("should make decide call to Amp agent and return array with all candidates", function(done) {
-        session.decide("DecideTest", {"tao":["awesome", "ok", "worthless"]}, {}, function(error, response) {
-            if (error) { console.log(error); }
+        session.decide("DecideTest", {"tao":["awesome", "ok", "worthless"]}, {}, function(error, decision, response) {
+            if (error) { 
+                console.log(error); 
+                fail();
+            }
             console.log(response); // response should be array
+
+            expect(decision.length).toEqual(3);
+            expect(decision[0].tao).toEqual("awesome");
+
             done();
         });
     });
