01b1633219f76051bf990abeda822db2662130c5
Steve Mask <steve@scaledinference.com>
2017-08-17 11:54:15 -0700

    Updated tests to check for array of candidates always being returned

--- spec/AmpSpec.js
+++ spec/AmpSpec.js
@@ -4,7 +4,7 @@ describe("Amp", function(){
     const request = require("request");
     request.debug = true;
 
-    var amp = new Amp( {key: "c5746225f9d4b191", userId: "ThinNodeTest", domain: "https://dev.amp.ai"} );
+    var amp = new Amp( {key: "561e88527bd0a73c", userId: "ThinNodeTest", domain: "https://dev.amp.ai"} );
     var session = new amp.Session();
 
     it("should make observe call to Amp agent", function(done) {
@@ -15,10 +15,18 @@ describe("Amp", function(){
         });
     });
 
-    it("should make decide call to Amp agent", function(done) {
+    it("should make decide call to Amp agent and return array with single candidate", function(done) {
+        session.decide("DecideTest", {"tao":["awesome", "ok", "worthless"]}, {limit: 1}, function(error, response) {
+            if (error) { console.log(error); }
+            console.log(response); // response should be array
+            done();
+        });
+    });
+
+    it("should make decide call to Amp agent and return array with all candidates", function(done) {
         session.decide("DecideTest", {"tao":["awesome", "ok", "worthless"]}, {}, function(error, response) {
             if (error) { console.log(error); }
-            console.log(response);
+            console.log(response); // response should be array
             done();
         });
     });
