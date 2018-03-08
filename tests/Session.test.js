654da020397e71cf8dcbd510b378b84cfcd0041c
Steve Mask <steve@scaledinference.com>
2018-03-08 12:46:31 -0700

    change key

--- tests/Session.test.js
+++ tests/Session.test.js
@@ -5,7 +5,7 @@ const Session = require("../Session");
 
 describe("Session", function(){
 
-    var amp = new Amp( {key: "561e88527bd0a73c", userId: "ThinNodeTest", domain: "https://dev.amp.ai", ttl: 5000} );
+    var amp = new Amp( {key: "ac646ba1c1398b12", userId: "ThinNodeTest", domain: "https://dev.amp.ai", ttl: 5000} );
     var session = new amp.Session();
 
     it("should make observe call to Amp agent", function(done) {
