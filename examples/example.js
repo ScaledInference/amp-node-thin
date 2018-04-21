11aed5005e8636f08ebf3b117459dd03a62b33cb
Steve Mask <steve@scaledinference.com>
2018-04-20 19:53:18 -0700

    readme

--- examples/example.js
+++ examples/example.js
@@ -76,6 +76,33 @@ Template Decide request sent! ${err ? "Error: " + err : " "} decide: ${JSON.stri
   `);
 });
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function demo() {
+    await sleep(1000);
+    // if you need to get all of the potential decisions because the context was not available and want to use that decision when it become available, you can use the conditional decide method
+    // by sending us the event and context you want decisions on along with your decision event name and candidates
+    session.decideCond('ConditionalDecide', {
+        color: ['red', 'green'],
+        font: ['bold', 'italic']
+    }, 'Locale', {en: {showModal: true}, es: {showModal: false}}, function (err, decision) {
+        if (err) {
+            console.log('ConditionalDecide conditional decision not sent!', err.message);
+        } else {
+            console.log('ConditionalDecide conditional decide sent!  Response was: ', decision);
+        }
+    });
+    await sleep(1000);
+    session.observe('Locale', {showModal: true}, function (err) {
+        if (err) {
+            console.log('Locale Observe not sent!', err.message);
+        } else {
+            console.log('Locale Observe request sent!');
+        }
+    });
+}
 
 // send another observe to observe user interaction to help improve decide
 // so we will build the model to help you make better decision on which template should be the best choice for which type of users and will give you the highest or lowest click on `SignUp`
