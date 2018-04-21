331daf14075051a97f984891af3a8eb7cd627ba2
Steve Mask <steve@scaledinference.com>
2018-04-20 20:24:23 -0700

    update/readme (#34)

--- examples/example.js
+++ examples/example.js
@@ -60,12 +60,53 @@ session.decide('TemplateCombo', {
   // now use the decision
   // decision.color
   // decision.font
-  if (err) {
-    console.log('TemplateCombo Decide not sent!', err.message);
-  } else {
-    console.log('TemplateCombo Decide request sent!', JSON.stringify(decision));
-  }
+  console.log(`
+Template Decide request sent! ${err ? "Error: " + err : " "} decide: ${JSON.stringify(decision)}
+  `);
 });
+
+// if you want to limit the number of candidates returned, pass a `limit` into the options
+session.decide("TemplateCombo", {
+  color: ["red", "green"],
+  font: ["bold", "italic"]
+}, {
+  limit: 2
+}, function(err, decision) {
+  // now use the decision
+  // decision.color
+  // decision.font
+  console.log(`
+Template Decide request sent! ${err ? "Error: " + err : " "} decide: ${JSON.stringify(decision)}
+  `);
+});
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function demo() {
+    await sleep(1000);
+    // if you need to get all of the potential decisions because the context was not available and want to use that decision when it become available, you can use the conditional decide method
+    // by sending us the event and context you want decisions on along with your decision event name and candidates
+    session.decideCond('ConditionalDecide', {
+        color: ['red', 'green'],
+        font: ['bold', 'italic']
+    }, 'Locale', {en: {showModal: true}, es: {showModal: false}}, function (err, decision) {
+        if (err) {
+            console.log('ConditionalDecide conditional decision not sent!', err.message);
+        } else {
+            console.log('ConditionalDecide conditional decide sent!  Response was: ', decision);
+        }
+    });
+    await sleep(1000);
+    session.observe('Locale', {showModal: true}, function (err) {
+        if (err) {
+            console.log('Locale Observe not sent!', err.message);
+        } else {
+            console.log('Locale Observe request sent!');
+        }
+    });
+}
 
 // send another observe to observe user interaction to help improve decide
 // so we will build the model to help you make better decision on which template should be the best choice for which type of users and will give you the highest or lowest click on `SignUp`
@@ -85,14 +126,14 @@ async function demo() {
     await sleep(1000);
     // if you need to get all of the potential decisions because the context was not available and want to use that decision when it become available, you can use the conditional decide method
     // by sending us the event and context you want decisions on along with your decision event name and candidates
-    session.decideCond('BlaBla', {
+    session.decideCond('ConditionalDecide', {
         color: ['red', 'green'],
         font: ['bold', 'italic']
     }, 'Locale', {en: {showModal: true}, es: {showModal: false}}, function (err, decision) {
         if (err) {
-            console.log('BlaBla conditional decision not sent!', err.message);
+            console.log('ConditionalDecide conditional decision not sent!', err.message);
         } else {
-            console.log('BlaBla conditional decide sent!  Response was: ', decision);
+            console.log('ConditionalDecide conditional decide sent!  Response was: ', decision);
         }
     });
     await sleep(1000);
